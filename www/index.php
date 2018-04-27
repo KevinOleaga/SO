1a9c9308305995a2cfc5afe1adc35bc195b3386a
Kevin Oleaga <oleaga@outlook.com>
2018-04-26 18:07:46 -0600

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Sistemas Operativos 2</title>
+	<title>Sistemas Operativos II</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
